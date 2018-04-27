5f79d93ce5c5eadedc8a4bcd58545480ef36eb19
Lukasz Zagroba <34895155+lzag@users.noreply.github.com>
2018-04-27 16:24:52 +0200

    -added Bootstrap styling to the menus, login and employer register forms

--- footer.php
+++ footer.php
@@ -2,27 +2,24 @@
 
 $year = date("Y",time());
 echo <<<_END
-<script
-			  src="http://code.jquery.com/jquery-3.3.1.min.js"
-			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-			  crossorigin="anonymous"></script>
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
-<div align="center">
-<footer class="fixed-bottom">
-<br>
-============================
-<br>
-Bottom Menu:
- <br>
- <div>
-  <p>Copyright: Lukasz Zagroba 2017 - $year</p>
-  <p>Contact information: <a href="mailto:contact@jojobboard.com">
- Contact@jojobboard.com</a>.</p>
- </div>
-</footer>
-</div>
+
+<footer>
+<div class="container">
+<div class="col-sm-6 m-auto text-center" >
+        <br>
+        ============================
+        <br>
+          <p>Copyright: Lukasz Zagroba 2017 - $year</p>
+          <p>Contact information: <a href="mailto:contact@jojobboard.com">
+         Contact@jojobboard.com</a>.</p>
+         </div>
+         </div>
+        </footer>
+
 </html>
 _END;
 
