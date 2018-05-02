54bc5aad8ae1256b013f024dc7b97acd847b6266
lukatastic <lzagroba@gmx.com>
2018-05-02 15:17:07 +0200

    - improved the login paged, added automatic redirect after inputting the login details

--- login.php
+++ login.php
@@ -4,32 +4,40 @@ $sitename = "Log In";
 
 require_once 'header.php';
 
+$msg = "Please input your login data:";
+
 if (isset($_POST['email']) &&
         isset($_POST['pass'])){
+
     $user = $_POST['email'];
     $pass = $_POST['pass'];
-    $conn = new Database();
     $query_login = "SELECT email, password FROM jjb_users WHERE email='$user' AND password='$pass'";
-    $result_check = $conn->execute_query($query_login);
+    $result_check = $db->execute_query($query_login);
     if ($result_check->num_rows == 1) {
         $_SESSION['user'] = $user;
         $_SESSION['pass'] = $pass;
-         echo "You are now logged in as a user. Please go to the <a href='index.php'>Main Page</a>";
+        header('Location: index.php');
+
     } else {
+
         $query_login = "SELECT contact_email, password FROM jjb_employers WHERE contact_email='$user' AND password='$pass'";
-        $result_check = $conn->execute_query($query_login);
+        $result_check = $db->execute_query($query_login);
             if ($result_check->num_rows == 1) {
                 $_SESSION['employer'] = $user;
                 $_SESSION['pass'] = $pass;
-                echo "You are now logged in as a employer. Please go to the <a href='index.php'>Main Page</a>";
-            } else echo "Username/password invalid";
+                header('Location: index.php');
+
+            } else {
+                $msg = "<span class='text-danger'>Username/password invalid.<br> Please try again:</span>";
+            }
     }
 }
 ?>
+
 <div class="container">
 <div class="row">
 <div class="col-sm-6 m-auto">
-<h2>Please input your login data: </h2>
+<h4><?php echo $msg; ?></h4>
 
 <form method="post" action='login.php'>
 <form-group>
