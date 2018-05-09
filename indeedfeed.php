a79c5f17d48d4deb18e046b8411d6f252bd63161
Lukasz Zagroba <34895155+lzag@users.noreply.github.com>
2018-05-09 16:31:13 +0200

    Adds more fields and fixes the date format

--- indeedfeed.php
+++ indeedfeed.php
@@ -1,21 +1,22 @@
 <?php
 require_once 'includes/database.php';
-
+$lastbuild = date("D, d M Y H:i:s T");
 $xml = <<<_END
 <?xml version="1.0" encoding="utf-8"?>
 <source>
 <publisher>JoJobBoard</publisher>
 <publisherurl>http://jojobboard.ga</publisherurl>
-
+<lastBuildDate>{$lastbuild}</lastBuildDate>
 _END;
 
 $sql = "SELECT posting_id, title, description, time_posted from jjb_postings";
 $result = $db->execute_query($sql);
 while($row = $result->fetch_assoc()) {
+$date = date("D, d M Y H:i:s T",strtotime($row['time_posted']));
 $xml .= <<<_END
 <job>
     <title><![CDATA[{$row['title']}]]></title>
-    <date><![CDATA[{$row['time_posted']}]]></date>
+    <date><![CDATA[{$date}]]></date>
     <referencenumber><![CDATA[{$row['posting_id']}]]></referencenumber>
     <url><![CDATA[http://www.jojobboard.ga/jobpostings.php?posting_id={$row['posting_id']}]]></url>
     <country><![CDATA[NOT YET AVAILABLE]]></country>
