870f797969ea3269cd14dab069f3a483b7d38ae1
Lukasz Zagroba <34895155+lzag@users.noreply.github.com>
2018-10-21 16:45:41 +0200

    Streamlined the search results query function

--- includes/jobpost.php
+++ includes/jobpost.php
@@ -26,129 +26,59 @@ class JobPost
     {
 
         global $db;
-
-        // initialize the filter variables
-        $filters = array(
-                    "location" => "",
-                    "salary_min" => "",
-                    "salary_max" => "",
-                    "keyword" => "",
-                    "id" => "",
-                    "order" => ""
-                    );
-
         // declare SQL filter rules
         $filter_rules = array(
                         "location" => " location LIKE '%%%s%%' ",
                         "salary_min" => " a.salary >=%d",
                         "salary_max" => " a.salary <=%d",
                         "keyword" => " (CONCAT(title, description) LIKE '%%%s%%') ",
-                        "id" => " a.posting_id=%d",
-                        "order" => " ORDER BY %s "
                         );
 
-        // check for possible filters in $_GET and assign them to the array
-        if (isset($_GET) && !empty($_GET)){
+        $query = "SELECT a.posting_id, a.title, b.company_name, a.description, a.time_posted, a.location, a.salary, a.local";
+        $query .= " FROM postings a ";
+        $query .= " INNER JOIN employers b ON a.employer_id=b.employer_id ";
 
-            if(isset($_GET['keyword'])) {
+        if (!empty($_GET)) {
 
-                $filters['keyword'] = explode(" ", $_GET['keyword']);
-            }
-        }
-
-    // GET THE FILTER VALUES AND UNASSIGN THE UNNECESSARY ONES
-        foreach($filters as $key => &$value) {
-            if (array_key_exists($key, $_GET) && !empty($_GET[$key])) {
-                if ($key == 'keyword') {
-                    continue;
-                } else {
-                    $value = $db->sanitize($_GET[$key]);
-                }
-
+            if (!empty($_GET['id'])) {
+                $query .= " WHERE a.posting_id=" . $_GET['id'];
             } else {
-                    unset($filters[$key]);
-                    unset($filter_rules[$key]);
-
-                }
-            }
-
-
-    // CHECK IF THE ORDER IS THERE AND GIVE IT A VALUE
-    if(isset($filters['order'])) {
-
-        $order = sprintf($filter_rules['order'],$filters['order']);
-        unset($filter_rules['order']);
-        unset($filters['order']);
-
-    }
-
-    $query = "SELECT a.posting_id, a.title, b.company_name, a.description, a.time_posted, a.location, a.salary, a.local";
-    $query .= " FROM postings a INNER JOIN employers b ON a.employer_id=b.employer_id  ";
-
-    // BUILD THE KEYWORD QUERY PART
-    if(isset($filters['keyword']) && !empty($filters['keyword'])) {
-
-        $keyword = "";
-        $last_keyword = end($filters['keyword']);
-        foreach($filters['keyword'] as $value) {
-
-            if ($value !== $last_keyword) {
-
-            $keyword .= sprintf($filter_rules['keyword'],$value);
-            $keyword .= " AND ";
-
-                } else {
-
-               $keyword .= sprintf($filter_rules['keyword'],$value);
-
+                $filters = [];
+                foreach ($_GET as $key => $value) {
+                    if (array_key_exists($key, $filter_rules) && !empty($value)) {
+                        $filters[$key] = $db->sanitize($value);
                     }
                 }
 
-             unset($filter_rules['keyword']);
-             unset($filters['keyword']);
+                if ($filters) {
+                    $query .= " WHERE ";
+                    foreach ($filters as $key => $value) {
+                        if ($key == "keyword") {
+                            $keywords = explode(" ", $_GET['keyword']);
+                            foreach($keywords as $value) {
+                                $query .= sprintf($filter_rules['keyword'],$value);
+                                $query .= ($value == end($keywords)) ? " " : " AND ";
+                            }
+                        } else {
+                            $query .= sprintf($filter_rules[$key],$_GET[$key]);
+                        }
+                        $query .= ($value == end($filters)) ? " " : " AND ";
+                    }
+                }
 
-    }
+                if(!empty($_GET['order'])) {
+                    $query .= " ORDER BY " . $_GET['order'];
+                }
 
-
-    // check if it's the single post page
-    if(!empty($filter_rules) || isset($keyword)) {
-
-       $query .= " WHERE ";
-
-        if (array_key_exists("id", $filter_rules)) {
-
-            $query .= sprintf($filter_rules['id'],$filters['id']);
-
-        } else {
-
-            $last_filter = end($filter_rules);
-            foreach($filter_rules as $key => $value) {
-
-                $query .= $filters[$key] ? sprintf($filter_rules[$key],$filters[$key])  : "";
-                $query .= ($filter_rules[$key] == $last_filter) ? "" : " AND ";
-            }
-
-            if ($keyword) {
-
-                $query .= $keyword ;
-
-            }
-
-            if (isset($order)) {
-
-                $query .= $order ;
-
+                if (isset($limit, $offset)) {
+                    $query .= " LIMIT $offset, $limit";
+                } elseif (isset($limit)) {
+                    $query .= " LIMIT $limit";
+                }
             }
         }
 
-    }
-
-    if (isset($limit, $offset)) {
-        $query .= " LIMIT $offset, $limit";
-    } elseif (isset($limit)) {
-        $query .= "LIMIT $limit";
-    }
-
+    echo $query;
     $stmt = $db->con->query($query);
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -162,9 +92,7 @@ class JobPost
         return $result = [];
 
         }
-
     }
-
 
 private static function get_backfill_filters($pagesize = 5, $start_num = 1) {
 
