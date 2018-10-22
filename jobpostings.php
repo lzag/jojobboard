75ee2a770310f03800401ee70c4585308f3324f1
lzag <lzagroba@gmx.com>
2018-10-22 18:10:45 +0200

    Code styling and cleaning on the SERP page

--- jobpostings.php
+++ jobpostings.php
@@ -1,7 +1,5 @@
 <?php require_once 'header.php'; ?>
 
-
-
 <div class="container">
 
     <div class="row">
@@ -30,25 +28,25 @@
                     <div class="form-group col-md-3">
                         <label for="order">Order by</label>
                         <select name="order" class="form-control" id="order">
-                            <option value="time_posted" <?= selected("order", "time_posted") ?>>Date</option>
-                            <option value="salary" <?= selected("order", "salary") ?>>Salary</option>
-                            <option value="title" <?= selected("order", "title") ?>>Relevance</option>
+                            <option value="time_posted" <?=selected("order", "time_posted") ?>>Date</option>
+                            <option value="salary" <?=selected("order", "salary") ?>>Salary</option>
+                            <option value="title" <?=selected("order", "title") ?>>Relevance</option>
                         </select>
                     </div>
                     <div class="form-group col-md-2">
                         <label for="order_type">ASC/DESC </label>
                         <select class="form-control" id="order_type" name="order_type">
-                        <option value="ASC" <?= selected("order_type", "ASC") ?>>ASC</option>
-                        <option value="DESC" <?= selected("order_type", "DESC") ?>>DESC</option>
+                            <option value="ASC" <?=selected("order_type", "ASC") ?>>ASC</option>
+                            <option value="DESC" <?=selected("order_type", "DESC") ?>>DESC</option>
                         </select>
                     </div>
                     <div class="form-group col-md-2">
                         <label for="per_page">Per page</label>
                         <select class="form-control" id="per_page" name="per_page">
-                        <option value="5" <?= selected("per_page", "5") ?>>5</option>
-                        <option value="10" <?= selected("per_page", "10") ?>>10</option>
-                        <option value="25" <?= selected("per_page", "25") ?>>25</option>
-                        <option value="50" <?= selected("per_page", "50") ?>>50</option>
+                            <option value="5" <?=selected("per_page", "5") ?>>5</option>
+                            <option value="10" <?=selected("per_page", "10") ?>>10</option>
+                            <option value="25" <?=selected("per_page", "25") ?>>25</option>
+                            <option value="50" <?=selected("per_page", "50") ?>>50</option>
                         </select>
                     </div>
                 </div>
@@ -59,25 +57,6 @@
         </div>
     </div>
 
-    <?php
-
-// GET THE RESULTS
-
-//if(!isset($_GET['id'])) {
-//    $local_hits = $results_local ? count($results_local) : 0;
-//    $results_backfill = JobPost::get_backfill(100 - $local_hits);
-//    $backfill_hits = $results_backfill ? $results_backfill->hits: 0;
-//}
-    ?>
-    <!--
-    <div class="row">
-        <div class="col-8 m-auto">
-            We've found
-            <?= $local_hits ?> local and
-            <?= $backfill_hits ?> external results for your query
-        </div>
-    </div>
--->
     <div class="row">
         <div class="col-8 m-auto">
             <?php $results = showResults() ?>
@@ -85,21 +64,16 @@
     </div>
     <div class="row">
         <div class="col-8 m-auto">
-
-            <?php
-         //echo isset($_GET["id"]) ? '<a href="jobpostings.php">Go back to all</a><br>' : ""; ?>
             <?php pagination($results['total_hits']);?>
         </div>
     </div>
-
     <div class="row">
         <div class="col-8 m-auto">
             <?php
-if(array_key_exists('local',$results)) :
-        foreach($results['local'] as $row) :
-        $status = $user->getAppStatus($row['posting_id']);
-?>
-
+            if (array_key_exists('local', $results)) :
+                foreach ($results['local'] as $row) :
+                    $status = $user->getAppStatus($row['posting_id']);
+            ?>
             <div class="card mx-auto my-2">
                 <div class="card-body">
                     <h3 class="card-title">
@@ -111,9 +85,10 @@ if(array_key_exists('local',$results)) :
                     <p class="card-text">
                         <?php echo $row['description']; ?>
                     </p>
-                    <?php if (isset($_SESSION['user']) && ($status)) :
-             echo "<h5 class='text-info'>Status: <span class='badge badge-info'>$status</span></h5>";
-        else : ?>
+                    <?php   if (isset($_SESSION['user']) && ($status)) :
+                                echo "<h5 class='text-info'>Status: <span class='badge badge-info'>$status</span></h5>";
+                            else :
+                    ?>
                     <form action="<?php echo isset($_GET[" id"]) ? "application.php" : "jobpostings.php" ; ?>" method="get">
                         <input type="hidden" name="id" value="<?php echo $row['posting_id']; ?>">
                         <input type="submit" value="<?php echo isset($_GET[" id"]) ? "Apply" : "See Details" ; ?>" class="btn btn-primary">
@@ -125,23 +100,21 @@ if(array_key_exists('local',$results)) :
                         <?php echo $row['posting_id']; ?>
                     </h6>
                     <h6 class="text-secondary">Posted on:
-                        <?php echo date("D d F Y",strtotime($row['time_posted'])); ?>
+                        <?php echo date("D d F Y", strtotime($row['time_posted'])); ?>
                     </h6>
-
                 </div>
             </div>
 
             <?php
-    endforeach;
-endif;
-?>
+                endforeach;
+            endif;
+            ?>
 
             <!--    GET THE BACKFILL OFFERS -->
             <?php
-if(array_key_exists('foreign',$results)) :
-    foreach($results['foreign'] as $value) :
-?>
-
+            if (array_key_exists('foreign', $results)) :
+                foreach ($results['foreign'] as $value) :
+            ?>
             <div class="card mx-auto my-2">
                 <div class="card-body">
                     <h3 class="card-title">
@@ -160,27 +133,14 @@ if(array_key_exists('foreign',$results)) :
                         <?php echo $value->site; ?>
                     </h6>
                     <h6 class="text-secondary">Posted on:
-                        <?php echo date("D d F Y",strtotime($value->date)); ?>
+                        <?php echo date("D d F Y", strtotime($value->date)); ?>
                     </h6>
-
                 </div>
             </div>
-
             <?php
-    endforeach;
-endif;
-?>
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="col w-75 m-auto">
-            <p>Order by:
-
-                <a href='<?php /*JobPost::add_url_filter("time_posted");*/ ?>'>Date</a>
-                <a href='<?php /*JobPost::add_url_filter("salary");*/ ?>'>Salary</a>
-
-            </p>
+                endforeach;
+            endif;
+            ?>
         </div>
     </div>
 
