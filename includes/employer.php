79f7919d4a41c9ce86ac2a31050b3eb9d75e475a
Lukasz Zagroba <34895155+lzag@users.noreply.github.com>
2018-10-23 11:28:38 +0200

    Clean up of the employer class

--- includes/employer.php
+++ includes/employer.php
@@ -1,92 +1,87 @@
 <?php
 
-class Employer {
+class Employer
+{
     public $employer_id;
-    var $tax_id;
-    var $company_name;
+    private $tax_id;
+    private $company_name;
     private $password;
-    var $contact_first_name;
-    var $contact_second_name;
-    var $contact_email;
-    var $city;
+    private $contact_first_name;
+    private $contact_second_name;
+    private $contact_email;
+    private $city;
     private $db;
 
-    function __construct() {
+    public function __construct()
+    {
         $this->db = new App\Database();
         $this->contact_email = $_SESSION['employer'];
-        $this->employer_id = $this->getEmployerID($this->contact_email);
+        $this->getEmployerDetails();
     }
 
-   public function create_employer_account($tax_id,$company_name,$password,$contact_first_name,$contact_second_name,$contact_email,$city){
-   }
+    private function getEmployerDetails()
+    {
+        global $db;
+        $sql  = "SELECT * ";
+        $sql .= " FROM employers ";
+        $sql .= " WHERE contact_email= ?";
+        $stmt = $db->con->prepare($sql);
+        $stmt->execute(array($this->contact_email));
+        if ($user_details = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            foreach ($user_details as $k => $p) {
+                if (property_exists('Employer', $k)) {
+                    $this->$k = $p;
+                }
+            }
+        }
+    }
 
-    public function getEmployerID($contact_email) {
-        $query = "SELECT employer_id FROM employers WHERE contact_email='$contact_email'";
+    public function getProperty($prop)
+    {
+        if ($prop && $this->$prop) {
+            return $this->$prop;
+        } else {
+            return "Empty";
+        }
+    }
+
+    public function getApplications($pid)
+    {
+        $query = "SELECT a.application_id as 'Application ID', a.posting_id as 'Posting ID'";
+        $query .= ", concat(b.first_name, ' ', b.second_name) as 'Candidate'";
+        $query .= ", b.email as 'Contact email', b.cv_file as 'Download CV'";
+        $query .= " FROM applications a";
+        $query .= " LEFT OUTER JOIN users b ON a.user_id=b.user_id";
+        $query .= "  WHERE a.posting_id=$pid";
         $result = $this->db->con->query($query);
-        $row = $result->fetch(PDO::FETCH_ASSOC);
-        return $row['employer_id'];
-    }
-
-     public function getTaxID($contact_email) {
-        $query = "SELECT tax_id FROM employers WHERE contact_email='$contact_email'";
-        $result = $this->db->execute_query($query);
-        $row = $result->fetch_array();
-        return $row['tax_id'];
-    }
-
-     public function getContactEmail($contact_email) {
-        $query = "SELECT contact_email FROM employers WHERE contact_email='$contact_email'";
-        $result = $this->db->execute_query($query);
-        $row = $result->fetch_array();
-        return $row['contact_email'];
-    }
-
-     public function getContactFirstName($contact_email) {
-        $query = "SELECT contact_first_name FROM employers WHERE contact_email='$contact_email'";
-        $result = $this->db->execute_query($query);
-        $row = $result->fetch_array();
-        return $row['contact_first_name'];
-    }
-
-     public function getContactSecondName($contact_email) {
-        $query = "SELECT contact_second_name FROM employers WHERE contact_email='$contact_email'";
-        $result = $this->db->execute_query($query);
-        $row = $result->fetch_array();
-        return $row['contact_second_name'];
-    }
-
-     public function getCity($contact_email) {
-        $query = "SELECT city FROM employers WHERE contact_email='$contact_email'";
-        $result = $this->db->execute_query($query);
-        $row = $result->fetch_array();
-        return $row['city'];
-}
-      function getApplications($pid) {
-    $query = "SELECT a.application_id as 'Application ID',a.posting_id as 'Posting ID', concat(b.first_name, ' ', b.second_name) as 'Candidate', b.email as 'Contact email', b.cv_file as 'Download CV' FROM applications a LEFT OUTER JOIN users b ON a.user_id=b.user_id WHERE a.posting_id=$pid";
-    $result = $this->db->execute_query($query);
-    return $result;
-}
-
-    function getPostings() {
-            $query = "SELECT p.posting_id as 'ID', p.title as 'Job Title', count(a.user_id) as 'Applications' FROM postings p
- 				LEFT OUTER JOIN applications a ON p.posting_id  = a.posting_id
-               	INNER JOIN employers e on e.employer_id = p.employer_id
-                WHERE e.contact_email='$this->contact_email'
-                group by ID";
-    $result = $this->db->execute_query($query);
-    return $result;
-    }
-
-    function reviewApp($pid,$status,$appid) {
-        $query = "UPDATE applications SET status='$status' WHERE application_id='$appid'";
-        $result = $this->db->execute_query($query);
         return $result;
     }
 
-     public function removeEmployer() {
+    public function getPostings()
+    {
+        $query  = "SELECT p.posting_id as 'ID', p.title as 'Job Title', count(a.user_id) as 'Applications'";
+        $query .= " FROM postings p ";
+        $query .= " LEFT OUTER JOIN applications a ON p.posting_id=a.posting_id ";
+        $query .= " INNER JOIN employers e on e.employer_id=p.employer_id";
+        $query .= " WHERE e.contact_email='$this->contact_email'";
+        $query .= " GROUP_BY ID";
+        $result = $this->db->con->query($query);
+        return $result;
+    }
+
+    public function reviewApp($pid, $status, $appid)
+    {
+        $query = "UPDATE applications SET status='$status' WHERE application_id='$appid'";
+        $result = $this->db->con->query($query);
+        return $result;
+    }
+
+    public function removeEmployer()
+    {
         $query = "DELETE FROM employers WHERE contact_email='$this->contact_email'";
-        $result = $this->db->execute_query($query);
-        if ($result) echo "Employer account removed";
+        $result = $this->db->con->query($query);
+        if ($result) {
+            echo "Employer account removed";
+        }
     }
 }
-
