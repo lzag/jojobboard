abd2f233584dd6f383e2bec23662c3edc698809e
Lukasz Zagroba <34895155+lzag@users.noreply.github.com>
2018-10-24 14:49:02 +0200

    Login page styling and clean up

--- login.php
+++ login.php
@@ -8,27 +8,28 @@ login();
 ?>
 
 <div class="container">
-<div class="row">
-<div class="col-sm-6 m-auto">
-
-<form method="POST" action='login.php'>
-<form-group>
-    <label for="email">Email</label>
-    <input type="text" name="email" placeholder="Your email address" class="form-control">
-</form-group>
-<br>
-<form-group>
-    <label for="password">Password</label>
-    <input type="password" name="pass" placeholder="Your password" class="form-control">
-    <span class="text-info"><a href="recover.php">Forgot your password? </a></span>
-</form-group>
-<br>
-<br>
-<input type="submit" name="login" value="Log in" class="btn btn-primary">
-</form>
-
-</div>
-</div>
+    <div class="row mt-3">
+        <div class="col-sm-6 m-auto">
+            <h3>Please log in</h3>
+            <form method="POST" action='login.php'>
+                <form-group>
+                    <label for="email">Email</label>
+                    <input type="text" name="email" placeholder="Your email address" class="form-control">
+                </form-group>
+                <br>
+                <form-group>
+                    <label for="password">Password</label>
+                    <input type="password" name="pass" placeholder="Your password" class="form-control">
+                    <span class="text-info"><a href="recover.php">Forgot your password? </a></span>
+                </form-group>
+                <div class="form-group form-check">
+                    <input name="rememberMe" type="checkbox" class="form-check-input" id="rememberMe">
+                    <label class="form-check-label" for="rememberMe">Keep me logged in on this machine</label>
+                </div>
+                <input type="submit" name="login" value="Log in" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
 </div>
 
 <?php require_once 'footer.php'; ?>
