fc8e53797ac09e6655e7932d60333bdae741baf6
Lukasz Zagroba <34895155+lzag@users.noreply.github.com>
2021-12-04 19:48:25 -0500

    Improves docker config

--- includes/constants.php
+++ includes/constants.php
@@ -3,8 +3,8 @@
 define('DB_USER', getenv('MYSQL_USER', true));
 define('DB_NAME', getenv('MYSQL_DATABASE', true));
 define('DB_PASS', getenv('MYSQL_PASSWORD', true));
-define('DB_HOST', 'db');
-define('DB_PORT', '3306');
+define('DB_HOST', getenv('MYSQL_HOST', true));
+define('DB_PORT', getenv('MYSQL_PORT'));
 
 # define main directories
 define('APP_DIR', __DIR__ . '/..');
